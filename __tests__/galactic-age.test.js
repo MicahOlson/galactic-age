51706fb15336f8c5501dc523f8e66380e1cd061b
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-02-05 14:57:15 -0800

    Add red-stage test for new person properties

--- __tests__/galactic-age.test.js
+++ __tests__/galactic-age.test.js
@@ -2,13 +2,18 @@ import { Person } from '../src/js/galactic-age.js';
 
 let person;
 beforeEach(() => {
-  person = new Person(30);
+  person = new Person(30, 'Male', 'Sedentary');
 });
 
 describe('Person', () => {
   test('should correctly construct a person object with an earth age', () => {
     expect(person.earthAge).toEqual(30);
   });
+
+  test('should correctly assign given gender and activity level to properties', () => {
+    expect(person.gender).toEqual('Male');
+    expect(person.activityLevel).toEqual('Sedentary');
+  })
 
   test('should correctly construct a person with galactic ages object', () => {
     expect(typeof(person.galacticAges)).toEqual('object');
@@ -36,11 +41,11 @@ describe('Person.setGalacticAges()', () => {
 describe('Person.getGalacticAge()', () => {
   test('should correctly return the age of the user on a given planet', () => {
     expect(person.getGalacticAge('Mercury')).toEqual('You are 125 years old on Mercury.');
-  })
+  });
 });
 
 describe('Person.setEarthLifeExpectancy', () => {
   test('should correctly set an earth-age life expectancy', () => {
     expect(person.setEarthLifeExpectancy('Male', 'Sedentary')).toEqual(73.1);
-  })
-})
+  });
+});
--- src/js/galactic-age.js
+++ src/js/galactic-age.js
@@ -37,5 +37,4 @@ export class Person {
     }
     return earthLifeExpectancy;
   }
-
 }
