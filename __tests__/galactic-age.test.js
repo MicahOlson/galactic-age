12c1d23a770f97002789e1517e5b5994fea9bec6
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-02-05 10:48:36 -0800

    Refactor tests with setup function

--- __tests__/galactic-age.test.js
+++ __tests__/galactic-age.test.js
@@ -1,12 +1,16 @@
 import { Person } from '../src/js/galactic-age.js';
 
+let person;
+beforeEach(() => {
+  person = new Person(30);
+});
+
 describe('Person', () => {
   test('should correctly construct a person object with an earth age', () => {
-    const person = new Person(30);
     expect(person.earthAge).toEqual(30);
   });
+  
   test('should correctly construct a person with galactic ages', () => {
-    const person = new Person(30);
     expect(person.galacticAges).toEqual({});
   });
 });
