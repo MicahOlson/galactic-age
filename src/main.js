882ca10bc6ce2f98fe96f22f9c87e3f8a1d3bd18
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-02-07 14:45:42 -0800

    Add 'show all' table

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Person from './js/galactic-age.js';
 
+let user;
 $("form#userInput").submit((event) => {
   event.preventDefault();
   const age = parseInt($("input#ageInput").val());
@@ -11,7 +12,16 @@ $("form#userInput").submit((event) => {
   const gender = $("input:radio[name=genderRadios]:checked").val();
   const activityLevel = $("input:radio[name=activityRadios]:checked").val();
   
-  const user = new Person(age, gender, activityLevel);
+  user = new Person(age, gender, activityLevel);
   $("#galacticAge").text(user.getGalacticAge(planet));
   $("#lifeExpectancyResult").text(user.getGalacticLifeExpectancy(planet));
+  $("#seeAllLink").show();
+});
+
+$("#seeAllLink").click(() => {
+  $("tr#appendedTableRows").remove();
+  $("#allStats").show();
+  for (const planet in user.galacticAges) {
+    $("#statsTable").append(`<tr id="appendedTableRows"><td>${planet}</td><td>${Math.round(user.galacticAges[planet])}</td><td>${Math.round(user.galacticLifeExpectancies[planet])}</td></tr>`);
+  }
 });
