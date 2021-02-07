628f3b058234071dff861af46054511add5474a7
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-02-07 13:18:34 -0800

    Add UI logic to get galactic age

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Person from './js/galactic-age.js';
+
+$("form#userInput").submit((event) => {
+  event.preventDefault();
+  const age = parseInt($("input#ageInput").val());
+  const planet = $("select#planetSelect").val();
+  const gender = $("input:radio[name=genderRadios]:checked").val();
+  const activityLevel = $("input:radio[name=activityRadios]:checked").val();
+  
+  const user = new Person(age, gender, activityLevel);
+  $("#result").text(user.getGalacticAge(planet));
+  
+});
