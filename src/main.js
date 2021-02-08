dfe7dc0a43172bafb44540c6a48b2851f25bf819
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-02-07 17:25:20 -0800

    Add years to live to table and tweak styles

--- src/main.js
+++ src/main.js
@@ -4,24 +4,27 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Person from './js/galactic-age.js';
 
-let user;
-$("form#userInput").submit((event) => {
-  event.preventDefault();
-  const age = parseInt($("input#ageInput").val());
-  const planet = $("select#planetSelect").val();
-  const gender = $("input:radio[name=genderRadios]:checked").val();
-  const activityLevel = $("input:radio[name=activityRadios]:checked").val();
-  
-  user = new Person(age, gender, activityLevel);
-  $("#galacticAge").text(user.getGalacticAge(planet));
-  $("#lifeExpectancyResult").text(user.getGalacticLifeExpectancy(planet));
-  $("#seeAllLink").show();
-});
+$(document).ready(() => {
+  let user;
+  $("form#userInput").submit((event) => {
+    event.preventDefault();
+    const age = parseInt($("input#ageInput").val());
+    const planet = $("select#planetSelect").val();
+    const gender = $("input:radio[name=genderRadios]:checked").val();
+    const activityLevel = $("input:radio[name=activityRadios]:checked").val();
 
-$("#seeAllLink").click(() => {
-  $("tr#appendedTableRows").remove();
-  $("#allStats").show();
-  for (const planet in user.galacticAges) {
-    $("#statsTable").append(`<tr id="appendedTableRows"><td>${planet}</td><td>${Math.round(user.galacticAges[planet])}</td><td>${Math.round(user.galacticLifeExpectancies[planet])}</td></tr>`);
-  }
+    user = new Person(age, gender, activityLevel);
+    $("#galacticAge").text(user.getGalacticAge(planet));
+    $("#lifeExpectancyResult").text(user.getGalacticLifeExpectancy(planet));
+    $("#results").show();
+    $("#seeAllLink").show();
+  });
+
+  $("#seeAllLink").click(() => {
+    $("tr#appendedTableRows").remove();
+    $("#statsTable").show();
+    for (const planet in user.galacticAges) {
+      $("#statsTable").append(`<tr id="appendedTableRows"><td>${planet}</td><td>${Math.round(user.galacticAges[planet])}</td><td>${Math.round(user.galacticLifeExpectancies[planet])}</td><td>${Math.round(user.galacticLifeExpectancies[planet] - user.galacticAges[planet])}</td></tr>`);
+    }
+  });
 });
